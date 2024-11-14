fb198264a01d0fab249f9afba5651a60e372dd8d
Lurius-Kitsune <lucasfox@outlook.fr>
2024-11-14 17:35:40 +0100

    -Cleaning

--- Prog1-SandBox/Main.cpp
+++ Prog1-SandBox/Main.cpp
@@ -8,9 +8,6 @@ using namespace Tools;
 int main()
 {
     FileStream _file = FileStream("bob.txt", true);
-	Image _image("cat.jpg", 700,700);
-	_image.ReadImage();
-	_image.CopyImage("cat2.jpg");
     //_file.Write("\nBob1", _file.ComputeLenghOfFile());
     Console::DisplayRainbowCenterMultiLine(_file.ReadAll(), _file.ComputeLineOfFile());
     //DISPLAY(_file.ReadAll(), true);
