2a94eda20d9d2ab08d1d042a2e0860a88b983f2d
ThiboRodenburger <113009654+ThiboRodenburger@users.noreply.github.com>
2024-11-25 10:37:43 +0100

    progress bar

---
+++ ProgressBar.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "ProgressBar.h"
+
+Tools::ProgressBar::SoundBar(const string& _name, const int _currentSound, const int _soundMax)
+{
+	name = _name;
+	currentSound = _currentSound;
+	soundMax = _soundMax;
+}
+
+void Tools::ProgressBar::IncreaseVolume()
+{
+	currentSound++;
+	if (currentSound >= 50)
+	{
+		currentSound = 50;
+	}
+}
+void Tools::ProgressBar::TurnDownVolume()
+{
+	currentSound--;
+	do
+	{
+		if (currentSound <= 0)
+		{
+			currentSound = 0;
+		}
+	} while (true);
+
+}
+
---
+++ ProgressBar.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+
+#ifdef MYTOOL_EXPORTS
+#define MYTOOL_API __declspec(dllexport)
+#else
+#define MYTOOL_API __declspec(dllimport)
+#endif
+
+
+#pragma warning(disable : 4251)
+
+using namespace std;
+
+#define CALBARD Sleep
+namespace Tools
+{
+	/// <summary>
+	/// Classe permettant de gérer une barre de progression
+	/// </summary>
+	class MYTOOL_API ProgressBar
+	{
+		string name;
+		int currentSound;
+		int soundMax;
+
+	public:
+		inline string GetName() const
+		{
+			return name;
+		}
+
+		inline int GetCurrentSound() const
+		{
+			return currentSound;
+		}
+
+	public:
+		SoundBar() = default;
+		SoundBar(const string& _name, const int _currentsound, const int _soundMax);
+
+	public:
+		void IncreaseVolume();
+
+		void TurnDownVolume();
+	
+	};
+}
+
